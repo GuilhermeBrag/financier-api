0ae7df9db1d5909e8d0ff9e8b1642a03839cdbc7
GuilhermeBrag <guibragadantas@hotmail.com>
2023-10-29 18:55:36 -0300

    first commit

---
+++ src/main/java/com/example/financierapi/FinancierApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.financierapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancierApiApplication.class, args);
+	}
+
+}
