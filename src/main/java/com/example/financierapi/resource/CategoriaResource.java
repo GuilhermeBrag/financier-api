92cc7f1604000e1f02447c09c408e5e27a6be7e4
GuilhermeBrag <guibragadantas@hotmail.com>
2023-10-30 22:01:32 -0300

    CRIAÇÃO DO “POST” PARA CRIAR UMA CATEGORIA E DO METODO “GET” PARA BUSCAR UMA CATEGORIA POR CODIGO.

--- src/main/java/com/example/financierapi/resource/CategoriaResource.java
+++ src/main/java/com/example/financierapi/resource/CategoriaResource.java
@@ -2,13 +2,17 @@ package com.example.financierapi.resource;
 
 import com.example.financierapi.model.Categoria;
 import com.example.financierapi.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -22,6 +26,21 @@ public class CategoriaResource {
         return categoriaRepository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<Categoria> criar (@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public Optional<Categoria> buscarPeloCodigo (@PathVariable Long codigo){
+        return categoriaRepository.findById(codigo);
+    }
 
 
 }
