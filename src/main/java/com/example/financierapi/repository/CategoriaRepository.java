2e4046cf967321fc04ecf1f4ea5372a094b8005a
GuilhermeBrag <guibragadantas@hotmail.com>
2023-10-30 21:27:21 -0300

    CRIAÇÃO DOS PACOTES DE MODEL, REPOSITORY E RESOURCE E O “GET” LISTAR PARA AS CATEGORIAS.

---
+++ src/main/java/com/example/financierapi/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.example.financierapi.repository;
+
+import com.example.financierapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/example/financierapi/resource/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.example.financierapi.resource;
+
+import com.example.financierapi.model.Categoria;
+import com.example.financierapi.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+
+
+
+}
